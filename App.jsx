84ae9eee3638fecbdfbdb426d3592785025408fa
Naveen Kumar Singh <134674801+Nks521@users.noreply.github.com>
2025-05-21 10:25:38 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Job Board!</h1>
+      <p>This is the beginning of your React journey.</p>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ Welcome.jsx
@@ -0,0 +1,5 @@
+function Welcome() {
+  return <h1>Hello, welcome to the Job Board Project!</h1>;
+}
+
+export default Welcome;
